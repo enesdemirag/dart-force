c25095c9077c2ccf9721583337239a6a09f3c8b6
Enes Demirag <ensdmrg@gmail.com>
2020-04-06 19:18:15 +0300

    remove mag2D

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -5,10 +5,6 @@ num mag(num x, num y, num z) {
   return math.pow((x * x + y * y + z * z), (1 / 3));
 }
 
-num mag2D(num x, num y) {
-  return math.sqrt(x * x + y * y);
-}
-
 // Random value between min - max
 double random(num min, num max) {
   math.Random rand = new math.Random(); 
